c40c2b34e29332c126baf1185a9b6a3441541819
willyan <willyanmarquesmelo@gmail.com>
2026-08-13 09:45:03 -0300

    initial setup from system module

---
+++ backend/api-transacoes/src/main/java/com/banco_real/api_transacoes/ApiTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.banco_real.api_transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacoesApplication.class, args);
+	}
+
+}
